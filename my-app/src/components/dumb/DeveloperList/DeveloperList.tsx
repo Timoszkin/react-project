1b2c19e75a8949e4b1148ba3a43d471820f93554
Olga T <jenflower97@gmail.com>
2021-11-26 14:45:15 +0400

    fix developer type

--- my-app/src/components/dumb/DeveloperList/DeveloperList.tsx
+++ my-app/src/components/dumb/DeveloperList/DeveloperList.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import "./DeveloperList.css";
 import Developer from "../Developer/Developer";
 
+type DeveloperType = { fullname: string; link: string };
+
 type DeveloperListProp = {
-  developers: Array<any>;
+  developers: Array<DeveloperType>;
 };
 
 function DeveloperList({ developers }: DeveloperListProp) {
   const devs = developers.map((dev) => (
-    <Developer fullname={dev.fullname} link={dev.link} />
+    <Developer key={dev.fullname} fullname={dev.fullname} link={dev.link} />
   ));
 
   return <ul className="dev-list">{devs}</ul>;
