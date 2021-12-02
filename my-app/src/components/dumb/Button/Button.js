5d1daf2d0a0e4f0af1d2bfb7c9e45a685de4346d
Olga T <46646722+JenFlower@users.noreply.github.com>
2021-12-02 15:00:25 +0400

    fix Button.tsx to Button.js for using PropTypes

---
+++ my-app/src/components/dumb/Button/Button.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import "./Button.css";
+
+function Button(props) {
+  const onClick = () => {
+    if (props.handleClick) {
+      props.handleClick();
+    }
+  };
+  return (
+    <button type="button" className="button" onClick={onClick}>
+      {props.text}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  handleClick: PropTypes.func
+}
+
+export default Button;
