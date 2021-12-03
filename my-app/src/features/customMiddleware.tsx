ad6db77eacc50cd5007659c50cc7b921d4701cc5
Olga T <46646722+JenFlower@users.noreply.github.com>
2021-12-03 22:10:49 +0400

    refactoring mware

--- my-app/src/features/customMiddleware.tsx
+++ my-app/src/features/customMiddleware.tsx
@@ -6,7 +6,7 @@ import { Middleware } from "redux";
 import { useNavigate } from "react-router";
 
 const userActionList = ['user/addFavorites', 'user/removeFavorites']
-const сustomMiddleware = (store: any) => (next: any) => (action: any) => {
+const сustomMiddleware: Middleware = (store) => (next) => (action) => {
 
   console.log('action: ', action.payload)
   const state = store.getState();
@@ -24,37 +24,9 @@ const сustomMiddleware = (store: any) => (next: any) => (action: any) => {
     }
   }
   else {
-    const navigate = useNavigate();
-    navigate('/signup');
+    // const navigate = useNavigate();
+    // navigate('/signup');ы
   }
 }
 
-export default сustomMiddleware
-
-// export const AUTH_ACTIONS = ["auth/ADDED_HISTORY", "auth/TOGGLED_FAV"];
-
-// const authGuard: Middleware = (store) => (next) => (action) => {
-//   const state = store.getState();
-//   const login = state.auth.login;
-//   const isAuthAction = AUTH_ACTIONS.includes(action.type);
-//   if (isAuthAction) {
-//     if (login) {
-//       const { toggleFav, addHistory } = useUserData(login);
-//       switch (action.type) {
-//         case "auth/ADDED_HISTORY": {
-//           addHistory(action.payload);
-//           break;
-//         }
-//         case "auth/TOGGLED_FAV": {
-//           toggleFav(action.payload.id, action.payload.fav);
-//           break;
-//         }
-//       }
-//     } else {
-//       return "/register";
-//     }
-//   }
-//   next(action);
-// };
-
-// export default authGuard;
+export default сustomMiddleware;
