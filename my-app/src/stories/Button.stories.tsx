2f3d31e34f3051d4a7bd938c4fb7398476d04d41
Olga T <46646722+JenFlower@users.noreply.github.com>
2021-12-05 02:09:14 +0400

    fix stories

--- my-app/src/stories/Button.stories.tsx
+++ my-app/src/stories/Button.stories.tsx
@@ -11,18 +11,29 @@ export default {
     },
     borderWidth: {
       control: {type: 'range', min: 0, max: 20, step: 1}
+    },
+    handleClick: {
+      action: 'clicked'
     }
   },
 } as ComponentMeta<typeof Button>
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-
-Primary.args = {
+export const Main = Template.bind({});
+Main.args = {
   text: 'Button',
   backgroundColor: 'transparent',
   borderColor: 'teal',
   color: 'teal'
+  // size: '10px'
+};
+
+export const Hovered = Template.bind({});
+Hovered.args = {
+  text: 'Button',
+  backgroundColor: 'teal',
+  borderColor: 'teal',
+  color: 'white'
   // size: '10px'
 };
--- my-app/src/stories/Footer.stories.tsx
+++ my-app/src/stories/Footer.stories.tsx
@@ -1,10 +1,17 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Footer from "../components/dumb/Footer/Footer"
 
+
 export default {
   title: 'Footer',
-  component: Footer
+  component: Footer,
 } as ComponentMeta<typeof Footer>
 
-export const Primary: ComponentStory<typeof Footer> = () => <Footer />;
- 
+const Template: ComponentStory<typeof Footer> = (args) => <Footer {...args} />;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+  backgroundColor: 'teal',
+  color: 'white'
+};
